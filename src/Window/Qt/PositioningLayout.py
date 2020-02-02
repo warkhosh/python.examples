25ca88656ee521836183b9a0e0f33469bc9a0f85
konstantinegorov <ekv.programmer@gmail.com>
2020-02-02 20:11:06 +0300

    add PositioningLayout.py

---
+++ src/Window/Qt/PositioningLayout.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+
+
+class PositioningLayout(QWidget):
+    def __init__(self, application):
+        super().__init__()
+        self._application = application
+        self.init_ui()
+
+    def init_ui(self):
+        self.move(300, 300)
+        self.resize(350, 250)
+        self.setWindowTitle('Window: positioning layout')
+
+        btn_ok = QPushButton("OK")
+        btn_cancel = QPushButton("Cancel")
+
+        horizontal_box = QHBoxLayout()
+        horizontal_box.addStretch(1)
+        horizontal_box.addWidget(btn_ok)
+        horizontal_box.addWidget(btn_cancel)
+
+        vertical_box = QVBoxLayout()
+        vertical_box.addStretch(1)
+        vertical_box.addLayout(horizontal_box)
+
+        self.setLayout(vertical_box)
+
+        self.show()
+        sys.exit(self.application.exec_())
+
+    @property
+    def application(self):
+        return self._application
+
+
+object_ = PositioningLayout(QApplication(sys.argv))
