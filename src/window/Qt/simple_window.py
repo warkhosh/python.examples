5d16d9b0fa7d3f39aa9c22a7228e8d5caf8b0b7b
konstantinegorov <ekv.programmer@gmail.com>
2020-02-02 17:04:14 +0300

    modify simple_window.py

--- src/window/Qt/simple_window.py
+++ src/window/Qt/simple_window.py
@@ -3,23 +3,24 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 
 def simple_window():
-    _window = QApplication(sys.argv)
-    w = QWidget(None)
-    w.resize(350, 250)
-    w.move(300, 300)
-    w.setWindowTitle('Simple window 350x250')
+    application = QApplication(sys.argv)
+    widget = QWidget()
+    widget.resize(350, 250)
+    widget.move(300, 300)
+    widget.setWindowTitle('Simple window 350x250')
 
     """
     Метод show() отображает виджет на экране. 
     Виджет сначала создаётся в памяти, и только потом (с помощью метода show) показывается на экране.
     """
-    w.show()
+    widget.show()
 
     """
     Основной цикл заканчивается, если мы вызываем метод exit() или главный виджет уничтожен. 
     Метод sys.exit() гарантирует чистый выход.
     """
-    sys.exit(_window.exec_())
+    sys.exit(application.exec_())
 
 
+# Процедурный тип
 simple_window()
