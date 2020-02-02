fda8f28f6c47a5dd04946a11bb384bac21551668
konstantinegorov <ekv.programmer@gmail.com>
2020-02-02 17:04:36 +0300

    add tooltip.py

---
+++ src/window/Qt/tooltip.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import (
+    QWidget, QToolTip, QPushButton, QApplication
+)
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QToolTip
+
+
+class Tooltip(QWidget):
+    def __init__(self, application):
+        super().__init__()
+        self._application = application
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.init_tooltip_option()
+
+        self.setToolTip('This is window')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <strong>QPushButton</strong> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.show()
+        sys.exit(self.application.exec_())
+
+    @staticmethod
+    def init_tooltip_option():
+        QToolTip.setFont(QFont('Verdana', 10))
+
+    @property
+    def application(self):
+        return self._application
+
+
+object_ = Tooltip(QApplication(sys.argv))
